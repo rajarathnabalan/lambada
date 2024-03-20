758594753513c4b0bd171e9a9cd2d672824eed43
Raja S <raja.s@continube.com>
2024-03-20 12:20:57 +0530

    V1 improvement

--- v1.go
+++ v1.go
@@ -22,11 +22,37 @@ func makeV1Request(ctx context.Context, req *Request) (*http.Request, error) {
 
 	// Update the request
 	httpReq.URL.Path = req.Path
-	httpReq.URL.RawQuery = url.Values(req.MultiValueQueryStringParameters).Encode()
-	httpReq.Header = canonicalizeHeader(req.MultiValueHeaders)
+
+	if len(req.MultiValueQueryStringParameters) == 0 && len(req.QueryStringParameters) > 0 {
+		httpReq.URL.RawQuery = toURLValues(req.QueryStringParameters).Encode()
+	} else {
+		httpReq.URL.RawQuery = url.Values(req.MultiValueQueryStringParameters).Encode()
+	}
+
+	if len(req.MultiValueHeaders) == 0 && len(req.Headers) > 0 {
+		httpReq.Header = fromSingleValueHeaders(req.Headers)
+	} else {
+		httpReq.Header = canonicalizeHeader(req.MultiValueHeaders)
+	}
+
+	if req.RequestContext.DomainName == "" {
+		req.RequestContext.DomainName = httpReq.Header.Get("host")
+		port := httpReq.Header.Get("x-forward-port")
+		if req.RequestContext.DomainName != "" && port != "" {
+			req.RequestContext.DomainName += ":" + port
+		}
+	}
 	httpReq.Host = req.RequestContext.DomainName
+
+	if req.RequestContext.Protocol == "" {
+		req.RequestContext.Protocol = httpReq.Header.Get("x-forward-proto")
+	}
 	httpReq.Proto = req.RequestContext.Protocol
 	httpReq.ProtoMajor, httpReq.ProtoMinor, _ = http.ParseHTTPVersion(req.RequestContext.Protocol)
+
+	if req.RequestContext.Identity.SourceIP == "" {
+		req.RequestContext.Identity.SourceIP = httpReq.Header.Get("x-forward-for")
+	}
 	httpReq.RemoteAddr = req.RequestContext.Identity.SourceIP
 
 	return httpReq, nil
