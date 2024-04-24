3203483a7f2fd716d8a4d1936a6dee461534277b
Raja S <raja.s@continube.com>
2024-04-24 14:48:21 +0530

    QueryUnescape added to toURLValues

--- values.go
+++ values.go
@@ -8,6 +8,10 @@ import (
 func toURLValues(v map[string]string) url.Values {
 	res := make(url.Values)
 	for k, v := range v {
+		newV, err := url.QueryUnescape(v)
+		if err == nil {
+			v = newV
+		}
 		res.Set(k, v)
 	}
 	return res
